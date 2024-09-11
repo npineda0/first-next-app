83388cf3159e24a0a33444d05a7cf8ab9dad1b19
npineda0 <97705502+npineda0@users.noreply.github.com>
2024-09-11 10:26:01 -0400

    3 products added and styled

--- src/app/page.js
+++ src/app/page.js
@@ -146,6 +146,38 @@ export default function Home() {
       <p> Times ive said nothing: {someNumber}</p>
       <p> Is it Chuesday, innit? {isItChuesday}</p>
       <button onClick={openModal}>view sun</button>
+
+       {/* product section */}
+       <div className={homeStyles.productContainer}>
+        {/* first product */}
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/e/e7/%22AM%22_%28Arctic_Monkeys%29.jpg'
+          height='200px' width='200px'
+          alt='AM'
+          />
+          <p>Arctic Monkeys - AM</p>
+          <p>$15.00</p>
+          <button>View</button>
+        </div>
+        {/* second product */}
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/f/f9/Suckitandsee.jpg'
+          height='200px' width='200px'
+          alt='Suck it and See'/>
+          <p>Arctic Monkeys - Suck it and See</p>
+          <p>$10.00</p>
+          <button>View</button>
+        </div>
+        {/* third product */}
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/en/2/20/Arcticmonkeys-humbug.jpg'
+          height='200px' width='200px'
+          alt='Suck it and See'/>
+          <p>Arctic Monkeys - Suck it and See</p>
+          <p>$10.00</p>
+          <button>View</button>
+        </div>
+      </div>
     </main>
   );
 }
